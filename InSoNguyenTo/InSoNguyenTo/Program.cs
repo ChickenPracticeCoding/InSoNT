f771e5b46bbb7bfc588d6f37c0296b7aa10fb0c1
ChickenPracticeCoding <trungkienhaui03@gmail.com>
2024-11-25 00:46:15 +0700

    ok

---
+++ InSoNguyenTo/InSoNguyenTo/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace InSoNguyenTo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InSoNT();
+        }
+        public static void InSoNT()
+        {
+            Console.WriteLine("Nhap n (Gioi han cua day so): ");
+            string str = Console.ReadLine();
+            bool passSucess = int.TryParse(str, out int n);
+            if( passSucess ) 
+            {
+                int dem = 0;
+                for (int i = 2; i <= n; i++)
+                {
+                    for(int j = 1; j <= i; j++)
+                    {
+                        if( i % j == 0 )
+                        {
+                            dem += 1;
+                        }  
+                    }    
+                    if (dem == 2)
+                    {
+                        Console.WriteLine(i);
+                    }
+                    dem = 0;
+                }
+            }
+            else 
+            {
+                Console.WriteLine("Loi nhap so nguyen cho n!");
+            }
+            
+        }
+    }
+}
